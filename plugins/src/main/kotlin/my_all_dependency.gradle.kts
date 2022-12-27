b2c9a7c917c8f1b51a1f1c61a87b2c786fb5e766
rohanghosh <rohangho@live.com>
2022-12-28 01:38:20 +0530

    added the task list which needed to be exposed

--- plugins/src/main/kotlin/my_all_dependency.gradle.kts
+++ plugins/src/main/kotlin/my_all_dependency.gradle.kts
@@ -1,3 +1,17 @@
 plugins {
     id ("org.jetbrains.kotlin.android")
 }
+
+val builderTaskGroup = "my sample project"
+tasks.named<TaskReportTask>("tasks")
+{
+    displayGroup = builderTaskGroup
+}
+
+tasks.getByName("build"){
+    group = builderTaskGroup
+}
+
+tasks.getByName("check"){
+    group = builderTaskGroup
+}
